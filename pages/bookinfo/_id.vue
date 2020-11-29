db049916cfb444127c2a8beb772355e7dc0f5cf4
全 <1583090876@qq.com>
2020-11-29 16:57:36 +0800

    bookinfo server

---
+++ pages/bookinfo/_id.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <el-row>
+      <el-col :span="4" :offset="2">
+        <h1>商品详情</h1>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="12" :offset="6">
+        <el-form ref="form" :model="book" label-width="80px">
+          <el-form-item label="商品id">
+            <el-input v-model="book.id" :disabled="true"></el-input>
+          </el-form-item>
+          <el-form-item label="商品名称">
+            <el-input v-model="book.name" :disabled="true"></el-input>
+          </el-form-item>
+          <el-form-item label="商品价格">
+            <el-input v-model="book.price" :disabled="true"></el-input>
+          </el-form-item>
+          <el-form-item label="商品库存">
+            <el-input v-model="book.count" :disabled="true"></el-input>
+          </el-form-item>
+          <el-form-item label="作者">
+            <el-input v-model="book.author" :disabled="true"></el-input>
+          </el-form-item>
+          <el-form-item label="出版社">
+            <el-input v-model="book.Press" :disabled="true"></el-input>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData ({ params, error }) {
+    try {
+      const { data } = await axios.get(`http://localhost:4000/book/${params.id}`)
+      // eslint-disable-next-line no-console
+      console.log(data)
+      return { book: data }
+    } catch (e) {
+      error({ statusCode: 404, message: `${params.id} 不存在` })
+    }
+  },
+  head: {
+    title: '商品详情',
+    meta: [
+      {hid: 'description', name: 'description', content: 'bookinfo page description'}
+    ]
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/bookinfo/index.vue
+++ pages/bookinfo/index.vue
@@ -1,13 +1,44 @@
 <template>
   <div>
-    <h1>
-      商品详情
-    </h1>
+    <el-row>
+      <el-col :span="4" :offset="2">
+        <h1>商品详情</h1>
+      </el-col>
+    </el-row>
+    <el-row style="margin-top: 20vh">
+      <el-col :span="6" :offset="8">
+        <el-input
+          placeholder="查询商品-输入商品id"
+          v-model="searchid"
+          clearable>
+        </el-input>
+      </el-col>
+      <el-col :span="2">
+        <el-button type="primary" @click="search">查询</el-button>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data:function() {
+    return{
+      searchid:''
+    }
+  },
+  methods:{
+    search:function () {
+      this.$router.push('/bookinfo/'+this.searchid)
+    }
+  },
+  head: {
+    title: '商品详情',
+    meta: [
+      {hid: 'description', name: 'description', content: 'bookinfo page description'}
+    ]
+  }
+}
 </script>
 
 <style scoped>
